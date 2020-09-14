9950c598beff010949728a19bfa3b6b544d6a9e7
root <root@DESKTOP-G0Q3ANV.localdomain>
2020-09-14 14:46:38 -0300

    :hammer: correction in controller Bakery

--- Bakery.Api/Controllers/BakeryController.cs
+++ Bakery.Api/Controllers/BakeryController.cs
@@ -11,11 +11,6 @@ namespace Bakery.Api.Controllers
     [Route("[controller]")]
     public class BakeryController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<BakeryController> _logger;
 
         public BakeryController(ILogger<BakeryController> logger)
@@ -24,16 +19,9 @@ namespace Bakery.Api.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Bakery> Get()
+        public async Task<ActionResult> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new Bakery
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok("Project Bakery.");
         }
     }
 }
