c82eb0af12bcee98c9e77e6f6c97c513096d0d69
Claudio Spinelli <claudiomanoel93@gmail.com>
2020-09-14 11:34:21 -0300

    ✔ Add project Gateway

---
+++ Gateway.Api/Controllers/GatewayController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Gateway.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GatewayController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<GatewayController> _logger;
+
+        public GatewayController(ILogger<GatewayController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Gateway> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Gateway
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
