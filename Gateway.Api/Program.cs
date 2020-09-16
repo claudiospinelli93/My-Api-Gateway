9195baf59718c200acc88533efdd2c038b1e5b01
claudio.spinelli <claudio.spinelli@sitemercado.com.br>
2020-09-15 22:35:12 -0300

    :satisfied: Remove unnecessary usings

--- Gateway.Api/Program.cs
+++ Gateway.Api/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Gateway.Api
 {
@@ -20,6 +16,8 @@ namespace Gateway.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    
+                  
                     webBuilder.UseStartup<Startup>();
                 });
     }
